52af9decc2f66c795496f6e24313c3b6b3bae7e4
peoush-04 <peoushchakraborty2903@gmail.com>
2024-09-17 15:41:14 +0530

    image slider

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/footer/Footer'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ImageSlider from './components/imageSlider/ImageSlider.jsx'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Footer />} />
+        <Route path="/slider" element={<ImageSlider/>} />
       </Routes>
     </BrowserRouter>
   );
